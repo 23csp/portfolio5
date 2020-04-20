7195ac39d34baa2207829306906790bc38cbc87c
23csp <57425602+23csp@users.noreply.github.com>
2020-04-19 22:01:10 -0700

    Added files from repl.it

---
+++ HTML-Project (2)/index.php
@@ -0,0 +1,16 @@
+<html>
+  <head>
+    <title>PHP Test</title>
+  </head>
+  <body>
+    <?php echo '<p>Hello World! Here are all of my Links [Noor Dharni Period 7]</p>'; ?> 
+    <ul>
+      <li><a href="index.html">Home</a></li></br>
+      <!-- Please make sure to change all the #'s to the proper link names-->
+      <li><a href="Last_AboutMe.html">About Me</a></li></br>
+      <li><a href="Last_Projects.html">Projects </a></li></br>
+      <li><a href="Last_Resume.html">Resume </a></li></br>
+      <li><a href="Last_ContactMe.html">Contact Me </a></li></br>
+    <ul>
+  </body>
+</html>
